f18fd0d20ecc7232ce83728ac230e48d24130113
linus muema <linus.m.muema@gmail.com>
2021-08-12 12:51:08 +0300

    - clear the project

--- app/src/main/java/com/moose/pets/MainActivity.kt
+++ app/src/main/java/com/moose/pets/MainActivity.kt
@@ -13,26 +13,6 @@ import com.moose.pets.ui.theme.PetsTheme
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
-            PetsTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
-                }
-            }
-        }
-    }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    PetsTheme {
-        Greeting("Android")
+        setContent { }
     }
 }
